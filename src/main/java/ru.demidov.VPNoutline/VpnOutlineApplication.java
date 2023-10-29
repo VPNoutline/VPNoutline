1af06ba5bba97c13af83e5fd95f61ebb3000a104
JavaDen44 <dem.denis44@gmail.com>
2023-10-29 16:09:12 +0300

    Создал основной класс VpnOutlineApplication и VpnOutlineApplicationTest

---
+++ src/main/java/ru.demidov.VPNoutline/VpnOutlineApplication.java
@@ -0,0 +1,13 @@
+package ru.demidov.VPNoutline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnOutlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VpnOutlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/demidov/VPNoutline/VpnOutlineApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.demidov.VPNoutline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class VpnOutlineApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
